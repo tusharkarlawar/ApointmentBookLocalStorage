f3252a2e915c66828441bfd452b60110772666aa
tusharkarlawar <tusharkarlawar95@gmail.com>
2023-05-26 15:23:23 +0530

    appointmentbook

---
+++ main.js
@@ -0,0 +1,31 @@
+function q(event){   
+    event.preventDefault() 
+let ob = { 
+       
+      fname: document.getElementById("fname").value, 
+      email: document.getElementById("email").value, 
+      phone: document.getElementById("phone").value, 
+       
+} 
+     //localstorage
+    localStorage.setItem(ob.email, JSON.stringify(ob)) 
+    
+    addd(ob) 
+}     
+     
+     
+ 
+function addd(ob) { 
+        let ul = document.getElementById('ulul'); 
+        let li = document.createElement('li'); 
+        let nText = ob.fname 
+        let nEmail = ob.email 
+        let nPhone = ob.phone 
+        let nNode = document.createTextNode(`${nText} - ${nEmail} - ${nPhone}`) 
+        li.appendChild(nNode) 
+        ul.appendChild(li) 
+
+         
+    } 
+
+    
